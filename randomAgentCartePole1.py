6b4456af6f5a7416b027fbc75544e8bcceef5996
Mohamad Nesr <71082961+MohamadNesr@users.noreply.github.com>
2022-10-18 17:05:39 +0200

    Video save

--- randomAgentCartePole1.py
+++ randomAgentCartePole1.py
@@ -1,7 +1,8 @@
 import gym
 import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v1', render_mode="human")
+env = gym.make('CartPole-v1', render_mode="rgb_array")
+env = gym.wrappers.RecordVideo(env, video_folder="vid")
 env.action_space.seed(42)
 nb_episodes = 1
 reward_cumul = 0
@@ -13,21 +14,17 @@ y = []
 for _ in range(1000):
     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
     reward_cumul += reward
+
     if terminated or truncated:
-        observation, info = env.reset()
         nb_episodes += 1
         x.append(nb_episodes)
         y.append(reward_cumul)
         reward_cumul = 0
+        observation, info = env.reset()
+env.close()
 
-print("cart position " + str(observation[0:]))
-print("cart velocity " + str(observation[1:]))
-print("pole angle " + str(observation[2:]))
-print("pole angular velocity " + str(observation[3:]))
-print(y)
 plt.plot(x, y)
 plt.ylabel("rewards")
 plt.xlabel("episodes")
 plt.show()
 
-env.close()
